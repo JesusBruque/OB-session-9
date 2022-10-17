438f734bfed9d84f663ea56171b2550caab3228a
JesusBruque <bruke3_voleyutrera@hotmail.com>
2022-10-17 11:56:24 +0200

    list contact

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import TaskComponent from '../Task/Task.jsx';
+import { Task } from "../../models/task.class";
+
+import "./taskList.css";
+
+const TaskList = () => {
+
+    const defaultTask1 = new Task('Jesús', 'Bruque', 'bruke3', true);
+    const defaultTask2 = new Task('Alba', 'Molina', 'aMolina', false);
+    const defaultTask3 = new Task('Sergio', 'Bruque', 'sergi1', true);
+
+    const [tasks, setTasks] = useState(defaultTask1, defaultTask2, defaultTask3);
+
+    useEffect(() => {
+    }, [tasks]);
+
+    return (
+        <div className="card">
+            <div className="card-header">
+                Contact List
+            </div>
+            <div className="card-body">
+                <table>
+                    <thead>
+                        <tr>
+                            <th scope='col'>Name</th>
+                            <th scope='col'>Last Name</th>
+                            <th scope='col'>UserName</th>
+                            <th scope='col'>Conected</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tasks.map = (task, index) => {
+                            return (
+                                <TaskComponent 
+                                    key={index}
+                                    task={task}
+                                />
+                            )
+                        }}
+                        {/* <TaskComponent task={defaultTask1}></TaskComponent> */}
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
+
+export default TaskList;
---
+++ src/models/task.class.js
@@ -0,0 +1,13 @@
+export class Task {
+    name = '';
+    lastName = '';
+    userName = '';
+    conected = true;
+
+    constructor(name, lastName, userName, conected){
+        this.name = name;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.conected = conected
+    }
+}
